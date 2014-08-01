a21f388476366ed727ba01f8eddbf640a95f8754
Paul Robertson <t.paulrobertson@gmail.com>
2014-07-31 22:36:03 -0400

    Fixed the order test

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,7 +3,32 @@ require 'test_helper'
 class OrderTest < ActiveSupport::TestCase
   test "create an order" do
     assert_difference "Order.all.count", 1 do
-      Order.create(character_name: "Gandhi", ship: ships(:naglfar))
+      order = Order.new valid_order
+      assert order.valid?, order.errors.messages
+      assert order.save
     end
   end
+
+  test "must contain order items" do
+    order = Order.new valid_order.except! :order_items_attributes
+    assert !order.valid?
+  end
+
+  test "must specify character name" do
+    order = Order.new valid_order.except! :character_name
+    assert !order.valid?
+  end
+
+  private
+
+    def valid_order
+      {
+        character_name: "Gandhi",
+        order_items_attributes: [{
+          item: ships(:naglfar),
+          quantity: 1
+        }]
+      }
+    end
+
 end
