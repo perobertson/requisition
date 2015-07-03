df63a9da6d540a8d3a8a1dfe75e5f0be3ee4206d
Paul Robertson <t.paulrobertson@gmail.com>
2015-07-03 19:40:23 -0400

    Bring over all the devise views and controllers for easy overriding

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    # omniauth_callbacks: 'users/omniauth_callbacks',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
 
   root 'purchases#index'
 
