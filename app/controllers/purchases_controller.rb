ba32a36b4d5e9902a9dcfc85ac1c9728744a63d5
Paul Robertson <t.paulrobertson@gmail.com>
2015-07-05 02:50:49 -0400

    fixed n+1 when loading the purchases page

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,9 @@
 class PurchasesController < ApplicationController
   def index
-    @items = Item.for_sale.order(:name).to_a
+    for_sale = Item.joins(:category).for_sale.order :name
+    @items = {}
+    Category.names.each do |name|
+      @items[name.to_sym] = for_sale.where categories: { name: name }
+    end
   end
 end
