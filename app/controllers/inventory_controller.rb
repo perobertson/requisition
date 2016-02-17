f39122018f95edebe1150a38a2cca6568d428996
Paul Robertson <t.paulrobertson@gmail.com>
2016-02-17 18:40:38 -0500

    fix layout on the inventory page

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -4,7 +4,7 @@ class InventoryController < ApplicationController
   def index
     return user_not_authorized unless current_user.can_view_inventory?
 
-    @items = Item.all.order(:name).to_a
+    @categories = Category.not_deleted.order(:name)
     @new_item = Item.new
   end
 end
