df7a48b3f219610e8bd8839febed6f60df103a93
Paul Robertson <t.paulrobertson@gmail.com>
2016-02-28 13:13:11 -0500

    added the user ability policy

---
+++ app/policies/user_ability_policy.rb
@@ -0,0 +1,23 @@
+class UserAbilityPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if @user && @user.can_view_users?
+        scope
+      else
+        scope.none
+      end
+    end
+  end
+
+  def show?
+    @user && @user.can_view_users?
+  end
+
+  def create?
+    @user && @user.can_change_user?
+  end
+
+  def destroy?
+    @user && @user.can_change_user?
+  end
+end
---
+++ test/policies/user_ability_policy_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class UserAbilityPolicyTest < ActiveSupport::TestCase
+  include Pundit::TestHelpers
+
+  subject { UserAbilityPolicy.new user, ability }
+
+  let(:user) { users(:user_no_abilities) }
+  let(:ability) { Ability.new }
+  let(:other_user) { User.new }
+
+  def test_scope
+    UserAbilityPolicy::Scope.new(user, UserAbility).resolve.count.must_equal UserAbility.none.count
+
+    user_with_abilities user, %i(view_users)
+    UserAbilityPolicy::Scope.new(user, UserAbility).resolve.count.must_equal UserAbility.all.count
+  end
+
+  describe 'no ability' do
+    it 'must not allow show' do
+      subject.show?.must_equal false
+    end
+
+    it 'must not allow create' do
+      subject.create?.must_equal false
+    end
+
+    it 'must not allow update' do
+      subject.update?.must_equal false
+    end
+
+    it 'must not allow destroy' do
+      subject.destroy?.must_equal false
+    end
+  end
+
+  describe 'has ability' do
+    it 'must allow show' do
+      user_with_abilities user, %i(view_users)
+      subject.show?.must_equal true
+    end
+
+    it 'must allow create' do
+      user_with_abilities user, %i(change_user)
+      subject.create?.must_equal true
+    end
+
+    it 'must not allow update' do
+      user_with_abilities user, Ability.KINDS
+      subject.update?.must_equal false
+    end
+
+    it 'must allow destroy' do
+      user_with_abilities user, %i(change_user)
+      subject.destroy?.must_equal true
+    end
+  end
+end
