57148ca859ce82f10aaf3460d69322da751bd08c
Paul Robertson <t.paulrobertson@gmail.com>
2016-03-05 15:59:24 -0500

    added links to signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def eve_sign_up_url
+    'https://secure.eveonline.com/trial/?invc=e69021a7-cab4-4518-b76a-db8033d4e057&action=buddy'
+  end
+
+  def eve_log_in_image_url
+    'https://images.contentful.com/idjq7aai9ylm/4fSjj56uD6CYwYyus4KmES/4f6385c91e6de56274d99496e6adebab/EVE_SSO_Login_Buttons_Large_Black.png?w=270&h=40'
+  end
 end
